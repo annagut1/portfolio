e48dcb7378d5b9a476da07bfd359ee42f55429cd
Yulia Gutorova <j.gutorova@gmail.com>
2023-11-23 17:44:26 +0100

    add function to show greeting

--- code.js
+++ code.js
@@ -1,7 +1,27 @@
 let seeLogoButton = document.querySelector('#seeLogoButton');
 let logoWrapper = document.querySelector('.logoWrapper');
+let homepageInput = document.querySelector('#homepageInput');
+let p = document.querySelector('#greetings');
 
-seeLogoButton.addEventListener('click', function() {
+
+//------------------ Function to show logo ------------------//
+function showLogo() {
     logoWrapper.style.visibility = 'visible';
     seeLogoButton.style.display = 'none';
-});
+}
+
+//------------------ Function to show greeting ------------------//
+function updateValue(e) {
+    
+    if (e.keyCode === 13) {
+        let name = homepageInput.value;
+        homepageInput.style.visibility = 'hidden';
+        p.innerText = "Welcome to my site, " + name;
+    }
+} 
+
+//-----------------------------------------------------------
+seeLogoButton.addEventListener('click', showLogo);
+homepageInput.addEventListener("keyup", updateValue);
+
+
